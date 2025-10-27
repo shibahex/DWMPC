4f6222f811024cce07ded518b6d712c65dc98945
Brandon <badamsva@gmail.com>
2025-10-26 22:40:30 -0400

    update dmw colors

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -7,15 +7,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=16" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const char col_fg_norm[]   = "#7dcfff";
+static const char col_bg_norm[]   = "#24283b";
+static const char col_fg_sel[]    = "#000000";
+static const char col_bg_sel[]    = "#7aa2f7";
+static const char col_border_sel[] = "#7aa2f7";
+static const char col_border_norm[] = "#24283b";
+
+static const char *colors[][3] = {
+	/*               fg           bg           border   */
+	[SchemeNorm] = { col_fg_norm, col_bg_norm, col_border_norm },
+	[SchemeSel]  = { col_fg_sel,  col_bg_sel,  col_border_sel  },
 };
 
 /* tagging */
@@ -58,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon}//, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon};//, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
